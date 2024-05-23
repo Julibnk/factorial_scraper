421ac509fb52e14f9c5d979d155776b0a0290ede
julibnk <fernandezfernandezjulian@gmail.com>
2024-05-23 09:55:43 +0200

    First version

--- main.py
+++ main.py
@@ -1,60 +1,58 @@
 import asyncio
+import os
 from playwright.async_api import async_playwright
-
 
 HEADLESS = False
 
 async def main():
     async with async_playwright() as p:
-        # Launch the browser
-        browser = await p.chromium.launch(headless=HEADLESS)
-        # Create a new browser context
-        context = await browser.new_context()
-        # Open a new page
-        page = await context.new_page()
-        # Go to the website
-        url = 'https://api.factorialhr.com/en/users/sign_in/'
-        await page.goto(url)
         
-        #Login
-        await page.get_by_title('Continue with Google').click()
-        
-        await page.locator('input[type="email"]').fill('julian.fernandez@enzyme.biz')
-        await page.get_by_text('Siguiente').click()
-        await page.locator('input[type="password"]').fill('yepalooo')
-        await page.get_by_text('Siguiente').click()
-        
-        locator = page.locator('#factorialRoot')
-        await locator.wait_for()
+        try:
+            # Launch the browser
+            browser = await p.chromium.launch(headless=HEADLESS)
+            # Create a new browser context
+            context = await browser.new_context()
+            # Open a new page
+            page = await context.new_page()
+            page.set_default_timeout(0)
+            # Go to the website
+            url = 'https://api.factorialhr.com/en/users/sign_in/'
+            await page.goto(url)
+            
+            #Login
+            await page.get_by_title('Continue with Google').click()
+            
+            await page.locator('input[type="email"]').fill(os.getenv("USER"))
+            await page.get_by_text('Siguiente').click()
+            await page.locator('input[type="password"]').fill(os.getenv("PASSWORD"))
+            await page.get_by_text('Siguiente').click()
+            
+            locator = page.locator('#factorialRoot')
+            await locator.wait_for()
+            
+            nav = page.get_by_role('navigation')
+            
+            await nav.get_by_text('Fichaje').click()
+            
+            table = page.get_by_role('table').first
+            await table.wait_for()
+            
+            for row in await table.get_by_role('row').filter(has_text='-8h').all():
+                open_row_button = row.get_by_role("button").last
+                await open_row_button.click()
+                new_row = table.get_by_role('row').filter(has_text='Añadir').first
+                await new_row.get_by_text('Añadir').click()
+                popover = page.locator('[data-radix-popper-content-wrapper]').first
+                await popover.get_by_placeholder('--:--').first.fill('09:00')
+                await popover.get_by_placeholder('--:--').last.fill('17:00')
+                await popover.get_by_text('Aplicar').click()
+                await open_row_button.click()
 
-
-        # Get the title of the page
-        title = await page.title()
-        print(f"Title of the page: {title}")
-        # Close the browser
-        await browser.close()
+            # Close the browser
+            await browser.close()
+        except Exception as e:
+            print(e)
+            await browser.close()
 
 # Run the main function
 asyncio.run(main())
-
-# option = webdriver.ChromeOptions()
-# option.add_argument("start-maximized")
-
-
-# # Install and set up Chrome WebDriver
-# service = Service(ChromeDriverManager().install(), options=option)
-# driver = webdriver.Chrome(service=service)
-
-
-
-
-# # Navigate to the webpage
-# driver.get(url)
-
-
-# sso_list_element = driver.find_element(By.CLASS_NAME, 'sso-list')
-# sso_list_element.find_elements(By.TAG_NAME, 'a')[0].click()
-
-
-# # Close the WebDriver
-# driver.quit()
